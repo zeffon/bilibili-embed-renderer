8267d8bf1976f0b5fb807a9c04a0f4ad56d3f3b8
Zeffon <ZeffonWu@gmail.com>
2022-06-26 19:57:25 +0800

    fix: package error

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -46,9 +46,7 @@ export default {
     copy({
       targets: [
         { src: 'src/bilibili-embed-renderer.css', dest: 'dist' },
-        { src: '../../LICENSE', dest: 'dist' },
-        { src: 'package.json', dest: 'dist' },
-        { src: 'README.md', dest: 'dist' }
+        { src: '../../LICENSE', dest: 'dist' }
       ]
     })
   ]
