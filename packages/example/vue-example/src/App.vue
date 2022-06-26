96537072e934c4aa28913e1d59fdbb44dc25d6bf
Zeffon <ZeffonWu@gmail.com>
2022-06-26 22:56:54 +0800

    feat: add vue-example

---
+++ packages/example/vue-example/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <BilibiliEmbedRenderer aid="3787944" :width="800" :height="600" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BilibiliEmbedRenderer from 'vue-bilibili-embed-renderer'
+import 'vue-bilibili-embed-renderer/dist/bilibili-embed-renderer.css'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
